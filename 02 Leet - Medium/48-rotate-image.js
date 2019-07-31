c25a2e186d6bae9bc166bf57b3fd17a01da970a7
Steven <stevenxchung@gmail.com>
2019-07-31 17:11:05 -0400

    Update 48-rotate-image.js

--- 02 Leet - Medium/48-rotate-image.js
+++ 02 Leet - Medium/48-rotate-image.js
@@ -7,3 +7,26 @@ Rotate the image by 90 degrees (clockwise).
 Note: You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 
 */
+
+let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+let rotate = matrix => {
+  // Assume N x N matrix
+  let mLen = matrix.length;
+
+  // Transpose matrix
+  for (let i = 0; i < mLen; i++) {
+    for (let j = i; j < mLen; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  // Reverse each row
+  for (let i = 0; i < mLen; i++) {
+    matrix[i] = matrix[i].reverse();
+  }
+
+  return matrix;
+};
+
+console.log(rotate(matrix));
