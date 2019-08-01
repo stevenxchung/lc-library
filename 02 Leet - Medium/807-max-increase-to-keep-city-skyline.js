452029c1769e514d3a9c28dc3e56ca4aae7bf3fa
Steven <stevenxchung@gmail.com>
2019-08-01 15:02:48 -0400

    Update 807-max-increase-to-keep-city-skyline.js

--- 02 Leet - Medium/807-max-increase-to-keep-city-skyline.js
+++ 02 Leet - Medium/807-max-increase-to-keep-city-skyline.js
@@ -73,37 +73,3 @@ let maxOutGrid = (grid, TB, LR, i, j) => {
 
 // Test
 console.log(maxGrid(grid));
-
-// Data structure for top and bottom skyline
-/*
-TB = [grid[i][0], grid[i][1], grid[i][2], grid[i][3]]
-*/
-
-// Data structure for left and right skyline
-/*
-LR = [grid[0][j], grid[1][j], grid[2][j], grid[3][j]]
-*/
-
-// Java Solution
-/*
-class Solution {
-    public int maxIncreaseKeepingSkyline(int[][] grid) {
-        int N = grid.length;
-        int[] rowMaxes = new int[N];
-        int[] colMaxes = new int[N];
-
-        for (int r = 0; r < N; ++r)
-            for (int c = 0; c < N; ++c) {
-                rowMaxes[r] = Math.max(rowMaxes[r], grid[r][c]);
-                colMaxes[c] = Math.max(colMaxes[c], grid[r][c]);
-        }
-
-        int ans = 0;
-        for (int r = 0; r < N; ++r)
-            for (int c = 0; c < N; ++c)
-                ans += Math.min(rowMaxes[r], colMaxes[c]) - grid[r][c];
-
-        return ans;
-    }
-}
-*/
