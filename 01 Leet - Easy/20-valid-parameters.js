77787136dc48acbc60920b7dc9ead1e40d902a40
Steven <stevenxchung@gmail.com>
2019-08-01 15:02:37 -0400

    Update 20-valid-parameters.js

--- 01 Leet - Easy/20-valid-parameters.js
+++ 01 Leet - Easy/20-valid-parameters.js
@@ -11,7 +11,7 @@ Note that an empty string is also considered valid.
 
 */
 
-let string = '[';
+let string = '()[]{}';
 
 let isValid = s => {
   // Initialize hash table of bracket types
@@ -24,8 +24,11 @@ let isValid = s => {
   s = [...s];
 
   for (let i = 0; i < s.length; i++) {
+    // Check if s[i] is in hash map
     if (bracketType[s[i]]) {
-      top = stack ? stack.pop() : '';
+      // Top is either the top of the stack or ''
+      top = stack.length !== 0 ? stack.pop() : '';
+      // Bracket does not match
       if (bracketType[s[i]] !== top) {
         return false;
       }
@@ -34,45 +37,12 @@ let isValid = s => {
     }
   }
 
-  return !stack;
+  // If stack is not empty then return false
+  if (stack.length !== 0) {
+    return false
+  }
+
+  return true;
 };
 
 console.log(isValid(string));
-
-/*
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-
-        # The stack to keep track of opening brackets.
-        stack = []
-
-        # Hash map for keeping track of mappings. This keeps the code very clean.
-        # Also makes adding more types of parenthesis easier
-        mapping = {")": "(", "}": "{", "]": "["}
-
-        # For every bracket in the expression.
-        for char in s:
-
-            # If the character is an closing bracket
-            if char in mapping:
-
-                # Pop the topmost element from the stack, if it is non empty
-                # Otherwise assign a dummy value of '#' to the top_element variable
-                top_element = stack.pop() if stack else '#'
-
-                # The mapping for the opening bracket in our hash and the top
-                # element of the stack don't match, return False
-                if mapping[char] != top_element:
-                    return False
-            else:
-                # We have an opening bracket, simply push it onto the stack.
-                stack.append(char)
-
-        # In the end, if the stack is empty, then we have a valid expression.
-        # The stack won't be empty for cases like ((()
-        return not stack
-*/
