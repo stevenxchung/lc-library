ac7a96e2dbdd3d10e96311fe0dd359c48a2676d7
Steven <stevenxchung@gmail.com>
2019-08-02 22:16:09 -0400

    Update 53-maximum-subarray.js

--- 01 Leet - Easy/53-maximum-subarray.js
+++ 01 Leet - Easy/53-maximum-subarray.js
@@ -6,3 +6,19 @@ If you have figured out the O(n) solution, try coding another solution using the
 
 */
 
+let array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+
+let maxSubArray = function(nums) {
+  let n = nums.length;
+  let maxSum = nums[0];
+    for(let i = 1; i < n; i++) {
+      if (nums[i - 1] > 0) {
+        nums[i] += nums[i - 1];
+        console.log('after', nums)
+      }
+      maxSum = Math.max(nums[i], maxSum);
+    }
+    return maxSum;
+};
+
+console.log(maxSubArray(array));
