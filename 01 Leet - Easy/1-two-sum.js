1887f0b4bf06d5f0f6b4fe577e3884fdb819f45f
Steven <stevenxchung@gmail.com>
2019-08-02 20:18:58 -0400

    Update 1-two-sum.js

--- 01 Leet - Easy/1-two-sum.js
+++ 01 Leet - Easy/1-two-sum.js
@@ -5,3 +5,37 @@ Given an array of integers, return indices of the two numbers such that they add
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 */
+
+let array = [2, 7, 11, 15];
+
+// let twoSum = (nums, target) => {
+//   let stack = [];
+//   let i = 0;
+
+//   while (i < nums.length) {
+//     // Search for second element
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (target === nums[i] + nums[j]) {
+//         stack.push(i, j);
+//       }
+//     }
+//     // Increment
+//     i++;
+//   }
+
+//   return stack;
+// };
+
+// Optimal using dictionary
+
+let twoSum = (nums, target) => {
+  let dict = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (dict[nums[i]] >= 0) {
+      return [dict[nums[i]], i]
+    }
+    dict[target - nums[i]] = i
+  }
+}
+
+console.log(twoSum(array, 26));
