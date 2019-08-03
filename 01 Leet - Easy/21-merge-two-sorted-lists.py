6028c71e7b7c3d2a42f566f690a43925c158cf65
Steven <stevenxchung@gmail.com>
2019-08-03 09:06:21 -0400

    Update 21-merge-two-sorted-lists.py

--- 01 Leet - Easy/21-merge-two-sorted-lists.py
+++ 01 Leet - Easy/21-merge-two-sorted-lists.py
@@ -3,3 +3,37 @@
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 
 '''
+
+
+class ListNode:
+    def __init__(self, data):
+        "constructor to initiate this object"
+        # store data
+        self.data = data
+        # store reference (next item)
+        self.next = None
+
+        return
+
+    def has_value(self, value):
+        "method to compare the value with the node data"
+        if self.data == value:
+            return True
+        else:
+            return False
+
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.value < l2.value:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+print(ListNode(1).has_value(1))
