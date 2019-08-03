9c5daea786949013cdccf1be1db58f157d8e7771
Steven <stevenxchung@gmail.com>
2019-08-02 20:22:49 -0400

    Create 53-maximum-subarray.js

---
+++ 01 Leet - Easy/53-maximum-subarray.js
@@ -0,0 +1,8 @@
+/*
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+*/
+
