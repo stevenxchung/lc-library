2b9661c11fab357c06633a1ce1da3b1106cd2da6
Steven <stevenxchung@gmail.com>
2019-08-04 18:26:45 -0400

    Create 2-add-two-numbers.py

---
+++ 02 Leet - Medium/2-add-two-numbers.py
@@ -0,0 +1,42 @@
+'''
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+'''
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        temp = ListNode(0)
+        temp.next = l1
+        print(temp.next)
+        self.help(l1, l2, temp, 0)
+        print(temp.next)
+        return temp.next
+
+    def help(self, l1, l2, temp, carry):
+        if l1 and l2:
+            sum = l1.val + l2.val + carry
+            l1.val = sum % 10
+            self.help(l1.next, l2.next, temp.next, sum//10)
+        elif l1:
+            sum = l1.val + carry
+            l1.val = sum % 10
+            self.help(l1.next, None, temp.next, sum//10)
+        elif l2:
+            sum = l2.val + carry
+            l2.val = sum % 10
+            temp.next = l2  # If l2 is truthy then set temp.next to l2
+            self.help(None, l2.next, temp.next, sum//10)
+        else:
+            if carry:
+                temp.next = ListNode(carry)
+                
