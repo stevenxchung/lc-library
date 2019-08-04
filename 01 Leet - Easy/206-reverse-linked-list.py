d13984bdc3b2248b888513480f14f1f17722d44c
Steven <stevenxchung@gmail.com>
2019-08-04 18:58:53 -0400

    Update 206-reverse-linked-list.py

--- 01 Leet - Easy/206-reverse-linked-list.py
+++ 01 Leet - Easy/206-reverse-linked-list.py
@@ -3,3 +3,20 @@
 Reverse a singly linked list.
 
 '''
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+      prev = None
+      while head:
+        temp = head
+        head = head.next
+        temp.next = prev
+        prev = temp
+      return prev
