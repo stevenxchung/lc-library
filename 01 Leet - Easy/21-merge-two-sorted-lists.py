63aae1f35ef6997e7707e97a4ec05cc52452b1ea
Steven <stevenxchung@gmail.com>
2019-08-05 19:56:26 -0400

    Update 21-merge-two-sorted-lists.py

--- 01 Leet - Easy/21-merge-two-sorted-lists.py
+++ 01 Leet - Easy/21-merge-two-sorted-lists.py
@@ -6,34 +6,24 @@ Merge two sorted linked lists and return it as a new list. The new list should b
 
 
 class ListNode:
-    def __init__(self, data):
-        "constructor to initiate this object"
-        # store data
-        self.data = data
-        # store reference (next item)
+    def __init__(self, x):
+        self.val = x
         self.next = None
-
-        return
-
-    def has_value(self, value):
-        "method to compare the value with the node data"
-        if self.data == value:
-            return True
-        else:
-            return False
 
 
 class Solution:
-    def mergeTwoLists(self, l1, l2):
-        if l1 is None:
-            return l2
-        elif l2 is None:
-            return l1
-        elif l1.value < l2.value:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
-        else:
-            l2.next = self.mergeTwoLists(l1, l2.next)
-            return l2
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode(-1)
+        prev = head
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            elif l2.val <= l1.val:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
 
-print(ListNode(1).has_value(1))
+        prev.next = l1 if l1 is not None else l2
+
+        return head.next
