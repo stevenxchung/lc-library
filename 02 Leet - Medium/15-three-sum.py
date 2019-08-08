0c9bae2ec2e4f04e82e72d63c1c4c0b3b4c78427
Steven <stevenxchung@gmail.com>
2019-08-08 18:16:59 -0400

    Update 15-three-sum.py

--- 02 Leet - Medium/15-three-sum.py
+++ 02 Leet - Medium/15-three-sum.py
@@ -5,3 +5,48 @@ Note:
 
 The solution set must not contain duplicate triplets.
 '''
+
+# nums = [-1, 0, 1, 2, -1, -4]
+# nums = [0, 0, 0]
+# nums = [0, 0]
+# nums = [1, 2, -2, -1]
+# nums = [1, -1, -1, 0]
+nums = [3, 0, -2, -1, 1, 2]
+
+
+class Solution:
+    def threeSum(self, nums):
+        # Initialize nums as sorted list
+        nums = sorted(nums)
+        # Initialize set since as set only contains unique triplets
+        solSet = set()
+        # Check for invalid inputs
+        if len(nums) < 3:
+            return solSet
+        print(nums)
+        for i, first in enumerate(nums):
+            if i != len(nums) - 1:
+                # Initialize pointers
+                index2, index3 = i + 1, len(nums) - 1
+            else:
+                break
+            # Target will be zero
+            target = -first
+            print(first, index2, index3)
+            # Loop until indices match
+            while index2 < index3:
+                twoSum = nums[index2] + nums[index3]
+                if target > twoSum:
+                    index2 += 1
+                elif target < twoSum:
+                    index3 -= 1
+                else:
+                    solSet.add((first, nums[index2], nums[index3]))
+                    index2 += 1
+                    index3 -= 1
+
+        return solSet
+
+
+sol = Solution()
+print(sol.threeSum(nums))
