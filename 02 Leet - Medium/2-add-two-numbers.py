f22b7d5d897845407e9ab3b75229c57f7043905a
Steven <stevenxchung@gmail.com>
2019-08-10 17:31:19 -0400

    Update 2-add-two-numbers.py

--- 02 Leet - Medium/2-add-two-numbers.py
+++ 02 Leet - Medium/2-add-two-numbers.py
@@ -11,30 +11,46 @@ class ListNode:
         self.next = None
 
 
-class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        temp = ListNode(0)
-        temp.next = l1
-        print(temp.next)
-        self.help(l1, l2, temp, 0)
-        print(temp.next)
-        return temp.next
+# class Solution:
+#     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+#         temp = ListNode(0)
+#         temp.next = l1
+#         print(temp.next)
+#         self.help(l1, l2, temp, 0)
+#         print(temp.next)
+#         return temp.next
 
-    def help(self, l1, l2, temp, carry):
-        if l1 and l2:
-            sum = l1.val + l2.val + carry
-            l1.val = sum % 10
-            self.help(l1.next, l2.next, temp.next, sum//10)
-        elif l1:
-            sum = l1.val + carry
-            l1.val = sum % 10
-            self.help(l1.next, None, temp.next, sum//10)
-        elif l2:
-            sum = l2.val + carry
-            l2.val = sum % 10
-            temp.next = l2  # If l2 is truthy then set temp.next to l2
-            self.help(None, l2.next, temp.next, sum//10)
-        else:
-            if carry:
-                temp.next = ListNode(carry)
-                
+#     def help(self, l1, l2, temp, carry):
+#         if l1 and l2:
+#             sum = l1.val + l2.val + carry
+#             l1.val = sum % 10
+#             self.help(l1.next, l2.next, temp.next, sum//10)
+#         elif l1:
+#             sum = l1.val + carry
+#             l1.val = sum % 10
+#             self.help(l1.next, None, temp.next, sum//10)
+#         elif l2:
+#             sum = l2.val + carry
+#             l2.val = sum % 10
+#             temp.next = l2  # If l2 is truthy then set temp.next to l2
+#             self.help(None, l2.next, temp.next, sum//10)
+#         else:
+#             if carry:
+#                 temp.next = ListNode(carry)
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        carry = 0
+        result = n = ListNode(0)
+        # While and of the below are true
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            carry, val = divmod(carry, 10)
+            print(carry, val)
+            n.next = n = ListNode(val)
+        return result.next
