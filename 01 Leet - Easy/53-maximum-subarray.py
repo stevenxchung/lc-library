94395724b12280f5b5bece16602ec3728448aba9
Steven <stevenxchung@gmail.com>
2019-08-14 19:19:10 -0400

    Update 53-maximum-subarray.py

--- 01 Leet - Easy/53-maximum-subarray.py
+++ 01 Leet - Easy/53-maximum-subarray.py
@@ -9,7 +9,15 @@ If you have figured out the O(n) solution, try coding another solution using the
 
 class Solution:
     def maxSubArray(self, nums):
-      
+        maxSum = nums[0]
+        for i in range(1, len(nums)):
+            # Only sum if positive, this way we gather the biggest sum
+            if nums[i - 1] > 0:
+                nums[i] += nums[i - 1]
+            # print('array:', nums)
+            maxSum = max(nums[i], maxSum)
+        return maxSum
+
 
 input = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 sol = Solution()
