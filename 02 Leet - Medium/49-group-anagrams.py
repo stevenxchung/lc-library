f45d661ecdb32086af8cb6e67a38c7a842855872
Steven <stevenxchung@gmail.com>
2019-08-15 19:50:59 -0400

    Update 49-group-anagrams.py

--- 02 Leet - Medium/49-group-anagrams.py
+++ 02 Leet - Medium/49-group-anagrams.py
@@ -5,6 +5,15 @@ Given an array of strings, group anagrams together.
 
 class Solution:
     def groupAnagrams(self, strs):
+        kVStore = {}
+        for str in strs:
+            key = ''.join(sorted(str))
+            if key not in kVStore:
+                kVStore[key] = [str]
+            else:
+                kVStore[key] += [str]
+
+        return kVStore.values()
 
 
 input = ["eat", "tea", "tan", "ate", "nat", "bat"]
