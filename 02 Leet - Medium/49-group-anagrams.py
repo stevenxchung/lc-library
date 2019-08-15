fba23b87d7d67d277ceca8e10a2cf669f124c341
Steven <stevenxchung@gmail.com>
2019-08-15 19:50:55 -0400

    Create 49-group-anagrams.py

---
+++ 02 Leet - Medium/49-group-anagrams.py
@@ -0,0 +1,12 @@
+'''
+Given an array of strings, group anagrams together.
+'''
+
+
+class Solution:
+    def groupAnagrams(self, strs):
+
+
+input = ["eat", "tea", "tan", "ate", "nat", "bat"]
+sol = Solution()
+print(sol.groupAnagrams(input))
