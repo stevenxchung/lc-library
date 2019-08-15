079ccb3644d278e399b73bddf34baf40085cdbb9
Steven <stevenxchung@gmail.com>
2019-08-14 20:44:50 -0400

    Update 937-reorder-log-files.py

--- 01 Leet - Easy/937-reorder-log-files.py
+++ 01 Leet - Easy/937-reorder-log-files.py
@@ -16,6 +16,17 @@ Return the final order of the logs.
 
 class Solution:
     def reorderLogFiles(self, logs):
+        digits = []
+        letters = []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+        
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        
+        return letters + digits
 
 
 input = ["a1 9 2 3 1", "g1 act car",
