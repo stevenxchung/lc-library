f723cf76390a369d2ba4605b6bfb0f90dee10186
Steven <stevenxchung@gmail.com>
2019-08-17 08:29:36 -0400

    Create 3-longest-substring-without-repeating-characters.py

---
+++ 02 Leet - Medium/3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,14 @@
+'''
+Given a string, find the length of the longest substring without repeating characters.
+'''
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+
+
+input = 'abcabcbb'
+# input = 'bbbbb'
+# input = 'pwwkew'
+sol - Solution()
+print(sol.lengthOfLongestSubstring(input))
