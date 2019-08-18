e62f531ffe95ace0a3960447cfe3f7432ed79733
Steven <stevenxchung@gmail.com>
2019-08-18 15:35:27 -0400

    Create 253-meeting-rooms-II.py

---
+++ 02 Leet - Medium/253-meeting-rooms-II.py
@@ -0,0 +1,12 @@
+'''
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+'''
+
+
+class Solution:
+    def minMeetingRooms(self, intervals):
+
+
+input = [[0, 30], [5, 10], [15, 20]]
+sol = Solution()
+print(sol.minMeetingRooms(input))
