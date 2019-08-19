b6ea9b6d53308eaeaaa991f8b91dc4c36ce92a0c
Steven <stevenxchung@gmail.com>
2019-08-19 18:38:17 -0400

    Update 253-meeting-rooms-II.py

--- 02 Leet - Medium/253-meeting-rooms-II.py
+++ 02 Leet - Medium/253-meeting-rooms-II.py
@@ -4,16 +4,19 @@ Given an array of meeting time intervals consisting of start and end times [[s1,
 
 class Solution:
     def minMeetingRooms(self, intervals):
-        e = ret = 0
-        start = sorted(i.start for i in intervals)
-        end = sorted(i.end for i in intervals)
+        # e is pointer to first available room
+        e = rooms = 0
+        # Intervals based on start times
+        start = sorted(i[0] for i in intervals)
+        # Intervals based on end times
+        end = sorted(i[1] for i in intervals)
         
         for s in range(len(start)):
             if start[s] < end[e]: 
-                ret += 1
+                rooms += 1
             else: 
                 e += 1
-        return ret
+        return rooms
 
 
 input = [[0, 30], [5, 10], [15, 20]]
