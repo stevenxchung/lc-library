47406932281622a45fa72d7f93f59d966c5310dc
Steven <stevenxchung@gmail.com>
2019-08-23 19:06:45 -0400

    Create 200-number-of-islands.py

---
+++ 02 Leet - Medium/200-number-of-islands.py
@@ -0,0 +1,17 @@
+'''
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+'''
+
+
+class Solution:
+    def numIslands(self, grid):
+
+
+input = [
+    ['1', '1', '1', '1', '0'],
+    ['1', '1', '0', '1', '0'],
+    ['1', '1', '0', '0', '0'],
+    ['0', '0', '0', '0', '0']
+]
+sol = Solution()
+print(sol.numIslands(input))
