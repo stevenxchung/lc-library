c449e1d25ff8bb24465f23ba603288b8790d459e
Steven <stevenxchung@gmail.com>
2019-08-23 19:06:49 -0400

    Update 200-number-of-islands.py

--- 02 Leet - Medium/200-number-of-islands.py
+++ 02 Leet - Medium/200-number-of-islands.py
@@ -5,6 +5,31 @@ Given a 2d grid map of '1's (land) and '0's (water), count the number of islands
 
 class Solution:
     def numIslands(self, grid):
+        # Check edge cases
+        if grid == None or len(grid) == 0:
+            return 0
+        # Initialize island count
+        islands = 0
+        # Trigger DFS when a '1' is hit
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[i])):
+                if grid[i][j] == '1':
+                    islands += self.dfsHelper(grid, i, j)
+
+        return islands
+
+    def dfsHelper(self, grid, i, j):
+        # Don't care if we are on a '0'
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == '0':
+            return 0
+        # Otherwise, traverse grid, set grid[i][j] to '0' to avoid traversing again
+        grid[i][j] = '0'
+        self.dfsHelper(grid, i + 1, j)
+        self.dfsHelper(grid, i - 1, j)
+        self.dfsHelper(grid, i, j + 1)
+        self.dfsHelper(grid, i, j - 1)
+
+        return 1
 
 
 input = [
