c5179e5be42dc59cb87d18aba61badd012fc0247
Steven <stevenxchung@gmail.com>
2019-09-04 21:03:39 -0400

    Update 279-perfect-squares.py

--- 02 Leet - Medium/279-perfect-squares.py
+++ 02 Leet - Medium/279-perfect-squares.py
@@ -5,8 +5,26 @@ Given a positive integer n, find the least number of perfect square numbers (for
 
 class Solution:
     def numSquares(self, n):
+        if n <= 0:
+            return 0
+
+        perfSquares = [0]
+
+        while len(perfSquares) <= n:
+            size = len(perfSquares)
+            squares = 2147483647
+
+            i = 1
+            while i * i <= size:
+                squares = min(squares, perfSquares[size - i * i] + 1)
+                i += 1
+
+            perfSquares.append(squares)
+
+        return perfSquares[n]
 
 
-input = 13
+input = 12
+# input = 13
 sol = Solution()
 print(sol.numSquares(input))
