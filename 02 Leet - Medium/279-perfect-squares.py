c1562dc98ec4f2875b6cc47991ba3b3b9b416f6d
Steven <stevenxchung@gmail.com>
2019-09-04 20:52:26 -0400

    Create 279-perfect-squares.py

---
+++ 02 Leet - Medium/279-perfect-squares.py
@@ -0,0 +1,12 @@
+'''
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+'''
+
+
+class Solution:
+    def numSquares(self, n):
+
+
+input = 13
+sol = Solution()
+print(sol.numSquares(input))
