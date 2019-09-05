ed8c1964c936c0485249a8b0960cd1d26bd19377
Steven <stevenxchung@gmail.com>
2019-09-05 18:41:58 -0400

    Update 279-perfect-squares.py

--- 02 Leet - Medium/279-perfect-squares.py
+++ 02 Leet - Medium/279-perfect-squares.py
@@ -16,12 +16,15 @@ Explanation: 13 = 4 + 9.
 
 
 class Solution:
+    # Dynamic programming
     def numSquares(self, n):
         if n <= 0:
             return 0
 
+        # Array of least number of perfect squares which sum to i for perfSquares[i]. If len(perfSquares) > 12 then we have already calculated the result
         perfSquares = [0]
 
+        # While len(perfSquares) <= n we can calculate next result until we get result for n
         while len(perfSquares) <= n:
             size = len(perfSquares)
             squares = 2147483647
@@ -35,8 +38,30 @@ class Solution:
 
         return perfSquares[n]
 
+    def numSquaresBFS(self, n):
+        dist = 1
+        q1 = [(0, 1)]
+        q2 = []
+        label = [10] * (n + 1)
+
+        # BFS using shortest path, length of all edges are 1 and graph has vertices which are numbers from 0 to n
+        while True:
+            for node, length in q1:
+                for edge in range(length, n + 1):
+                    nextNode = node + edge * edge
+                    if nextNode == n:
+                        return dist
+                    elif nextNode > n:
+                        break
+                    elif label[nextNode] > dist:
+                        label[nextNode] = dist
+                        q2.append((nextNode, edge))
+            q1, q2 = q2, []
+            dist += 1
+
 
 input = 12
 # input = 13
 sol = Solution()
 print(sol.numSquares(input))
+print(sol.numSquaresBFS(input))
