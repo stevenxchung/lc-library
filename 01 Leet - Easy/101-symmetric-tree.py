2a513c06d8f651237eed9ea7ee55964c8083d3e4
Steven <stevenxchung@gmail.com>
2019-09-19 13:50:09 -0400

    Update 101-symmetric-tree.py

--- 01 Leet - Easy/101-symmetric-tree.py
+++ 01 Leet - Easy/101-symmetric-tree.py
@@ -3,3 +3,31 @@ Given a binary tree, check whether it is a mirror of itself (ie, symmetric aroun
 
 For example, this binary tree [1,2,2,3,4,4,3] is symmetric
 '''
+
+
+class Node(object):
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def printTree(self):
+        print('--- Printing tree with preorder DFS ---')
+        self._preorderDFS(self)
+        print()
+
+    def _preorderDFS(self, node):
+        if node:
+            print(node.data, end=' ')
+            self._preorderDFS(node.left)
+            self._preorderDFS(node.right)
+
+
+left = Node(2)
+left.left = Node(3)
+left.right = Node(4)
+right = Node(2)
+right.right = Node(3)
+right.left = Node(4)
+node1 = Node(1, left, right)
+node1.printTree()
