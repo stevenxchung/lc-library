d6e0d0965318dc11a587e7948715ff1e2398010e
Steven <stevenxchung@gmail.com>
2019-09-19 13:50:18 -0400

    Update 101-symmetric-tree.py

--- 01 Leet - Easy/101-symmetric-tree.py
+++ 01 Leet - Easy/101-symmetric-tree.py
@@ -11,10 +11,22 @@ class Node(object):
         self.left = left
         self.right = right
 
+    def isSymmetric(self):
+        print('Is this tree symmetric?', end=' ')
+        print(self is None or self._isMirror(self.left, self.right))
+
     def printTree(self):
         print('--- Printing tree with preorder DFS ---')
         self._preorderDFS(self)
         print()
+
+    def _isMirror(self, left, right):
+        if left is None and right is None:
+            return True
+        elif left is not None and right is not None:
+            return left.data == right.data and self._isMirror(left.left, right.right) and self._isMirror(left.right, right.left)
+        else:
+            False
 
     def _preorderDFS(self, node):
         if node:
@@ -31,3 +43,4 @@ right.right = Node(3)
 right.left = Node(4)
 node1 = Node(1, left, right)
 node1.printTree()
+node1.isSymmetric()
