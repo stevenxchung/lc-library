a9d073c7d51f64860cd8bd61f10a24bcefd7c9ea
Steven <stevenxchung@gmail.com>
2019-09-20 13:49:29 -0400

    Update 104-maximum-depth-of-binary-tree.py

--- 01 Leet - Easy/104-maximum-depth-of-binary-tree.py
+++ 01 Leet - Easy/104-maximum-depth-of-binary-tree.py
@@ -5,3 +5,28 @@ The maximum depth is the number of nodes along the longest path from the root no
 
 Note: A leaf is a node with no children.
 '''
+
+
+class Node(object):
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def printTree(self):
+        print('--- Printing binary tree using preorder DFS')
+        self._preorderDFS(self)
+        print()
+
+    def _preorderDFS(self, node):
+        if node:
+            print(node.data, end=' ')
+            self._preorderDFS(node.left)
+            self._preorderDFS(node.right)
+
+head = Node(3)
+head.left = Node(9)
+head.right = Node(20)
+head.right.left = Node(15)
+head.right.right = Node(7)
+head.printTree()
