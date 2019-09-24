543e02652549f0ad286b3ab7eda44ca4b64a9a9c
Steven <stevenxchung@gmail.com>
2019-09-24 17:38:56 -0400

    Update 101-symmetric-tree.py

--- 01 Leet - Easy/101-symmetric-tree.py
+++ 01 Leet - Easy/101-symmetric-tree.py
@@ -5,62 +5,62 @@ For example, this binary tree [1,2,2,3,4,4,3] is symmetric
 '''
 
 
-# class Node(object):
-#     def __init__(self, data=None, left=None, right=None):
-#         self.data = data
-#         self.left = left
-#         self.right = right
+class Node(object):
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
 
-#     def isSymmetric(self):
-#         print('Is this tree symmetric?', end=' ')
-#         print(self is None or self._isMirror(self.left, self.right))
+    def isSymmetric(self):
+        print('Is this tree symmetric?', end=' ')
+        print(self is None or self._isMirror(self.left, self.right))
 
-#     def printTree(self):
-#         print('--- Printing tree with preorder DFS ---')
-#         self._preorderDFS(self)
-#         print()
-
-#     def _isMirror(self, left, right):
-#         # Leaf node reached
-#         if left is None and right is None:
-#             return True
-#         # Run symmetric checks
-#         elif left is not None and right is not None:
-#             return left.data == right.data and self._isMirror(left.left, right.right) and self._isMirror(left.right, right.left)
-#         # Otherwise, tree must not be symmetric
-#         else:
-#             False
-
-#     def _preorderDFS(self, node):
-#         if node:
-#             print(node.data, end=' ')
-#             self._preorderDFS(node.left)
-#             self._preorderDFS(node.right)
-
-
-# left = Node(2)
-# left.left = Node(3)
-# left.right = Node(4)
-# right = Node(2)
-# right.right = Node(3)
-# right.left = Node(4)
-# node1 = Node(1, left, right)
-# node1.printTree()
-# node1.isSymmetric()
-
-class Solution:
-    def isSymmetric(self, root):
-        # Tree is symmetric if there is no tree
-        if root is None:
-          return True
-        # Otherwise, defer to helper function to check for symmetry
-        else:
-          return self._isMirror(root.left, root.right)
+    def printTree(self):
+        print('--- Printing tree with preorder DFS ---')
+        self._preorderDFS(self)
+        print()
 
     def _isMirror(self, left, right):
+        # Leaf node reached
         if left is None and right is None:
             return True
+        # Run symmetric checks
         elif left is not None and right is not None:
-            return left.val == right.val and self._isMirror(left.left, right.right) and self._isMirror(left.right, right.left)
+            return left.data == right.data and self._isMirror(left.left, right.right) and self._isMirror(left.right, right.left)
+        # Otherwise, tree must not be symmetric
         else:
-            return False
+            False
+
+    def _preorderDFS(self, node):
+        if node:
+            print(node.data, end=' ')
+            self._preorderDFS(node.left)
+            self._preorderDFS(node.right)
+
+
+left = Node(2)
+left.left = Node(3)
+left.right = Node(4)
+right = Node(2)
+right.right = Node(3)
+right.left = Node(4)
+node1 = Node(1, left, right)
+node1.printTree()
+node1.isSymmetric()
+
+# class Solution:
+#     def isSymmetric(self, root):
+#         # Tree is symmetric if there is no tree
+#         if root is None:
+#           return True
+#         # Otherwise, defer to helper function to check for symmetry
+#         else:
+#           return self._isMirror(root.left, root.right)
+
+#     def _isMirror(self, left, right):
+#         if left is None and right is None:
+#             return True
+#         elif left is not None and right is not None:
+#             return left.val == right.val and self._isMirror(left.left, right.right) and self._isMirror(left.right, right.left)
+#         else:
+#             return False
