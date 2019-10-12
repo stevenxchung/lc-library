a9f9dc7c43ac601d1200d3a1267921f9f51b546d
Steven <stevenxchung@gmail.com>
2019-10-12 08:50:32 -0400

    Create 114-flatten-binary-tree-to-linked-list.py

---
+++ 02 Leet - Medium/114-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,35 @@
+'''
+Given a binary tree, flatten it to a linked list in-place.
+'''
+
+
+class Node:
+    def __init__(self, val=None, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    # Using preorder DFS
+    def printTree(self):
+        node = self
+        if node is None:
+            return
+
+        stack = []
+        stack.append(node)
+        print('Preorder DFS:', end=' ')
+        while len(stack) > 0:
+            node = stack.pop()
+            print(node.val, end=' ')
+
+            if node.right is not None:
+                stack.append(node.right)
+            if node.left is not None:
+                stack.append(node.left)
+
+
+# Test
+node2 = Node(2, Node(3), Node(4))
+node5 = Node(5, None, Node(6))
+head = Node(1, node2, node5)
+head.printTree()  # 1, 2, 3, 4, 5, 6
