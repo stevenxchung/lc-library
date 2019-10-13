a3cad33ad106829727eb2000a472c43e7a7c5261
Steven <stevenxchung@gmail.com>
2019-10-13 11:03:38 -0400

    Update 114-flatten-binary-tree-to-linked-list.py

--- 02 Leet - Medium/114-flatten-binary-tree-to-linked-list.py
+++ 02 Leet - Medium/114-flatten-binary-tree-to-linked-list.py
@@ -9,21 +9,6 @@ class Node:
         self.left = left
         self.right = right
 
-
-class Solution:
-    def __init__(self):
-        self.prev = None
-
-    def flatten(self, node):
-        if node is None:
-            return
-        self.flatten(node.right)
-        self.flatten(node.left)
-
-        node.right = self.prev
-        node.left = None
-        self.prev = node
-
     # Using preorder DFS
     def printTree(self):
         node = self
@@ -43,12 +28,29 @@ class Solution:
                 stack.append(node.left)
 
 
+class Solution:
+    def __init__(self):
+        self.prev = None
+
+    def flatten(self, node):
+        if node is None:
+            return
+        self.flatten(node.right)
+        self.flatten(node.left)
+
+        node.right = self.prev
+        node.left = None
+        self.prev = node
+
+
 # Test
-node2 = Node(2, Node(3), Node(4))
-node5 = Node(5, None, Node(6))
-head = Node(1, node2, node5)
-head.printTree()  # 1, 2, 3, 4, 5, 6
+# node2 = Node(2, Node(3), Node(4))
+# node5 = Node(5, None, Node(6))
+# head = Node(1, node2, node5)
+# head.printTree()  # 1, 2, 3, 4, 5, 6
+
+head = Node(1, Node(3), Node(4))
+head.printTree()  # 1, 3, 4
 
 sol = Solution()
-head = sol.flatten(head)
-head.printTree()
+sol.flatten(head)
