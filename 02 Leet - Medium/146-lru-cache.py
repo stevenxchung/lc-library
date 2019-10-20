1e6ef4c6458ec443c503a3af66031d4d6acea8d9
Steven <stevenxchung@gmail.com>
2019-10-20 16:18:08 -0400

    Update 146-lru-cache.py

--- 02 Leet - Medium/146-lru-cache.py
+++ 02 Leet - Medium/146-lru-cache.py
@@ -80,34 +80,3 @@ Could you do both operations in O(1) time complexity?
 #         else:
 #             node.value = value
 #             self.to_front(node)
-
-class DNode:
-    def __init__(self, key=0, val=0, prev=None, next=None):
-        self.key = key
-        self.val = val
-        self.prev = prev
-        self.next = next
-
-
-class LRUCache:
-    def __init__(self, capacity=0):
-        # Will be a stack
-        self.cache = {}
-        self.size = 0
-        self.capacity = capacity
-        self.head, self.tail = DNode(), DNode()
-
-        self.head.next, self.tail.prev = self.tail, self.head
-
-
-# Test
-cache = LRUCache()
-cache.put(1, 1)
-cache.put(2, 2)
-cache.get(1)       # returns 1
-cache.put(3, 3)    # evicts key 2
-cache.get(2)       # returns -1 (not found)
-cache.put(4, 4)    # evicts key 1
-cache.get(1)       # returns -1 (not found)
-cache.get(3)       # returns 3
-cache.get(4)       # returns 4
