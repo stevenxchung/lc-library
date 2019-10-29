f7ea6f62532efa0ce33d73a3ec767f17031f0e5b
Steven <stevenxchung@gmail.com>
2019-10-29 10:10:05 -0400

    Update 103-binary-tree-zigzag-level-order-traversal.py

--- 02 Leet - Medium/103-binary-tree-zigzag-level-order-traversal.py
+++ 02 Leet - Medium/103-binary-tree-zigzag-level-order-traversal.py
@@ -9,6 +9,27 @@ class Node:
         self.left = left
         self.right = right
 
+    # Level Order
+
+    def printBST(self):
+        node = self
+        queue = []
+        queue.append(node)
+
+        print("Printing BST:", end=' ')
+        while len(queue) > 0:
+
+            print(queue[0].val, end=' ')
+            node = queue.pop(0)
+
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+
+        print()
+
 
 # Test
 head = Node(3, Node(9), Node(20, Node(15), Node(7)))
+head.printBST()
