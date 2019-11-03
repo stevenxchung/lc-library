71b71fa0fd35e9f7c7a58f95395ce3adb9a5bd37
Steven <stevenxchung@gmail.com>
2019-11-03 08:53:57 -0500

    Update 103-binary-tree-zigzag-level-order-traversal.py

--- 02 Leet - Medium/103-binary-tree-zigzag-level-order-traversal.py
+++ 02 Leet - Medium/103-binary-tree-zigzag-level-order-traversal.py
@@ -28,8 +28,48 @@ class Node:
 
         print()
 
+    def printZigZag(self):
+        node = self
+        queue = []
+        queue.append(node)
+        count = 0
+
+        print("Printing BST:", end=' ')
+        while len(queue) > 0:
+
+            # Initial print at head node
+            if count == 0:
+                print(queue[0].val, end=' ')
+                node = queue.pop(0)
+
+                if node.right is not None:
+                    queue.append(node.right)
+                if node.left is not None:
+                    queue.append(node.left)
+
+            elif count % 2 != 0:
+                print(queue[0].val, end=' ')
+                node = queue.pop(0)
+
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+            else:
+                print(queue[0].val, end=' ')
+                node = queue.pop(0)
+
+                if node.right is not None:
+                    queue.append(node.right)
+                if node.left is not None:
+                    queue.append(node.left)
+
+            count += 1
+
+        print()
+
 
 # Test
 head = Node(3, Node(9), Node(20, Node(15), Node(7)))
 head.printBST() # 3 9 20 15 7
-# head.printZigZag() 3 20 9 15 7
+head.printZigZag() # 3 20 9 15 7
