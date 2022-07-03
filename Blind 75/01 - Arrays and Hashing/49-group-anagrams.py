c5a7e6dca902537b04854f47ac55ed9b77f398eb
Steven <stevenxchung@gmail.com>
2022-07-03 06:29:47 -0400

    Create 49-group-anagrams.py

---
+++ Blind 75/01 - Arrays and Hashing/49-group-anagrams.py
@@ -0,0 +1,55 @@
+'''
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+'''
+import collections
+from time import time
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        buckets = {}
+        for word in strs:
+            # Initialize map
+            a2z_hash_key = [0] * 26
+            for l in word:
+                a2z_hash_key[ord(l) - ord('a')] += 1
+
+            a2z_hash_key = str(a2z_hash_key)
+            if a2z_hash_key not in buckets:
+                buckets[a2z_hash_key] = [word]
+            else:
+                buckets[a2z_hash_key].append(word)
+
+        return [group for group in buckets.values()]
+
+    def reference(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        ["eat", "tea", "tan", "ate", "nat", "bat"],
+        [""],
+        ["a"],
+    ]
+
+    sol_start = time()
+    for case in test_cases:
+        print(test.groupAnagrams(case))
+    print(f'Runtime for our solution: {time() - sol_start}')
+
+    ref_start = time()
+    for case in test_cases:
+        print(test.reference(case))
+    print(f'Runtime for reference: {time() - ref_start}')
