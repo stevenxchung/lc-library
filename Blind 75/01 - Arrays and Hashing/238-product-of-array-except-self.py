657cea10079bcdec2a2f61d6bf2d5dc93fcad5fc
Steven <stevenxchung@gmail.com>
2022-07-05 08:02:59 -0400

    Create 238-product-of-array-except-self.py

---
+++ Blind 75/01 - Arrays and Hashing/238-product-of-array-except-self.py
@@ -0,0 +1,69 @@
+'''
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+'''
+from time import time
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = 1
+        suffix = 1
+
+        # Forward loop
+        output = {}
+        for i, e in enumerate(nums):
+            output[i] = prefix
+            prefix *= e
+
+        # Reverse loop
+        for i, e in reversed(list(enumerate(nums))):
+            output[i] *= suffix
+            suffix *= e
+
+        return output.values()
+
+    def reference(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.productExceptSelf(case))
+                else:
+                    self.productExceptSelf(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        [1, 2, 3, 4],
+        [-1, 1, 0, -3, 3]
+    ]
+    test.quantify(test_cases)
