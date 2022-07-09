7cf04e5d8241651c1bfb6a459edacc235a4a8ef5
Steven <stevenxchung@gmail.com>
2022-07-09 06:46:04 -0400

    Create 141-linked-list-cycle.py

---
+++ Blind 75/06 - Linked List/141-linked-list-cycle.py
@@ -0,0 +1,72 @@
+'''
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+'''
+from time import time
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        p1 = p2 = head
+        while p2 and p2.next:
+            p1 = p1.next
+            p2 = p2.next.next
+            if p1 == p2:
+                return True
+
+        return False
+
+    def reference(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.hasCycle(case))
+                else:
+                    self.hasCycle(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    t1 = ListNode(3, ListNode(2, ListNode(0)))
+    temp = ListNode(-4, t1.next)
+    t1.next.next.next = temp
+
+    t2 = ListNode(1, ListNode(2))
+    t2.next.next = t2
+    test_cases = [
+        t1,
+        t2,
+        ListNode(1),
+    ]
+    test.quantify(test_cases)
