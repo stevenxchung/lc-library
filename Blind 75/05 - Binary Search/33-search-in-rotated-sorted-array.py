99a1b3f1563e23422a2df1f91710c8085e6dfabc
Steven <stevenxchung@gmail.com>
2022-07-09 06:46:02 -0400

    Create 33-search-in-rotated-sorted-array.py

---
+++ Blind 75/05 - Binary Search/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,87 @@
+'''
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+'''
+from time import time
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = (i + j) // 2
+            if target == nums[m]:
+                return m
+
+            # Compare with middle value first
+            if nums[i] <= nums[m]:
+                # Left sorted
+                if target > nums[m] or target < nums[i]:
+                    i = m + 1
+                else:
+                    j = m - 1
+            else:
+                # Right sorted
+                if target < nums[m] or target > nums[j]:
+                    j = m - 1
+                else:
+                    i = m + 1
+
+        return -1
+
+    def reference(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            # left sorted portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            # right sorted portion
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.search(case[0], case[1]))
+                else:
+                    self.search(case[0], case[1])
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case[0], case[1]))
+                else:
+                    self.reference(case[0], case[1])
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        ([4, 5, 6, 7, 0, 1, 2], 0),
+        ([4, 5, 6, 7, 0, 1, 2], 3),
+        ([1], 0),
+    ]
+    test.quantify(test_cases)
