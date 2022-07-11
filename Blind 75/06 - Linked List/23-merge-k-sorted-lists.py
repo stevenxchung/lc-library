0ad875eb26e95fbddef4d99326fb6880bbdf41a5
Steven <stevenxchung@gmail.com>
2022-07-11 08:28:22 -0400

    Create 23-merge-k-sorted-lists.py

---
+++ Blind 75/06 - Linked List/23-merge-k-sorted-lists.py
@@ -0,0 +1,96 @@
+'''
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+'''
+from time import time
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode],
+                      list2: Optional[ListNode]) -> Optional[ListNode]:
+        node = ListNode()
+        p1 = node
+        while list1 and list2:
+            if list1.val < list2.val:
+                p1.next = list1
+                list1 = list1.next
+            else:
+                p1.next = list2
+                list2 = list2.next
+            p1 = p1.next
+
+        if list1:
+            p1.next = list1
+        elif list2:
+            p1.next = list2
+
+        return node.next
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # Handle edge case
+        if not lists or len(lists) == 0:
+            return None
+
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeTwoLists(l1, l2))
+            lists = mergedLists
+
+        return lists[0]
+
+    def reference(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+        return lists[0]
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.mergeKLists(case))
+                else:
+                    self.mergeKLists(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        [
+            ListNode(1, ListNode(4, ListNode(5))),
+            ListNode(1, ListNode(3, ListNode(4))),
+            ListNode(2, ListNode(6))
+        ],
+        [],
+        [[]]
+    ]
+    test.quantify(test_cases)
