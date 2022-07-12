ea77d016951cc22921352a386d23aeec35481b80
Steven <stevenxchung@gmail.com>
2022-07-12 08:27:24 -0400

    Create 226-invert-binary-tree.py

---
+++ Blind 75/07 - Trees/226-invert-binary-tree.py
@@ -0,0 +1,72 @@
+'''
+Given the root of a binary tree, invert the tree, and return its root.
+'''
+from time import time
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
+    def reference(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        # swap the children
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.reference(root.left)
+        self.reference(root.right)
+        return root
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.invertTree(case))
+                else:
+                    self.invertTree(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        TreeNode(4,
+                 TreeNode(2, TreeNode(1), TreeNode(3)),
+                 TreeNode(7, TreeNode(6), TreeNode(9)),
+                 ),
+        TreeNode(2, TreeNode(1), TreeNode(3)),
+        None
+    ]
+    test.quantify(test_cases)
