ea029702b4d22d3efde877cfd0ca4ebf3f528294
Steven <stevenxchung@gmail.com>
2022-07-11 21:47:14 -0400

    Update 104-maximum-depth-of-binary-tree.py

--- Blind 75/07 - Trees/104-maximum-depth-of-binary-tree.py
+++ Blind 75/07 - Trees/104-maximum-depth-of-binary-tree.py
@@ -1,5 +1,7 @@
 '''
-Given the root of a binary tree, invert the tree, and return its root.
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 '''
 from time import time
 from typing import Optional
