8f4f571503d2929effcdd3c96905481415e60ad7
Steven <stevenxchung@gmail.com>
2022-07-17 06:15:55 -0400

    Update 79-word-search.py

--- Blind 75/10 - Backtracking/79-word-search.py
+++ Blind 75/10 - Backtracking/79-word-search.py
@@ -42,7 +42,28 @@ class Solution:
         return False
 
     def reference(self, board: List[List[str]], word: str) -> bool:
-        return
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (min(r, c) < 0 or r >= ROWS or c >= COLS or
+                    word[i] != board[r][c] or (r, c) in path):
+                return False
+            path.add((r, c))
+            res = (dfs(r + 1, c, i + 1) or
+                   dfs(r - 1, c, i + 1) or
+                   dfs(r, c + 1, i + 1) or
+                   dfs(r, c - 1, i + 1))
+            path.remove((r, c))
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
+        return False
 
     def quantify(self, test_cases, runs=100000):
         sol_start = time()
