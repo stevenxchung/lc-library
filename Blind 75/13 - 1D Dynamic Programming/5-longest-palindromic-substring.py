b11a16c42ef020e0ada957173229eb8ba7cce3e2
Steven <stevenxchung@gmail.com>
2022-07-21 09:04:35 -0400

    Create 5-longest-palindromic-substring.py

---
+++ Blind 75/13 - 1D Dynamic Programming/5-longest-palindromic-substring.py
@@ -0,0 +1,79 @@
+'''
+Given a string s, return the longest palindromic substring in s.
+'''
+from time import time
+
+
+class Solution:
+    def helper(self, s: str, l: int, r: int) -> str:
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l + 1: r]
+
+    def longestPalindrome(self, s: str) -> str:
+        res = ''
+        for i in range(len(s)):
+            # Odd string
+            palindrome = self.helper(s, i, i)
+            if len(palindrome) > len(res):
+                res = palindrome
+            # Even string
+            palindrome = self.helper(s, i, i + 1)
+            if len(palindrome) > len(res):
+                res = palindrome
+
+        return res
+
+    def reference(self, s: str) -> str:
+        res = ""
+        resLen = 0
+
+        for i in range(len(s)):
+            # Odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l: r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # Even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l: r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+        return res
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.longestPalindrome(case))
+                else:
+                    self.longestPalindrome(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        'babad',
+        'cbbd'
+    ]
+    test.quantify(test_cases)
