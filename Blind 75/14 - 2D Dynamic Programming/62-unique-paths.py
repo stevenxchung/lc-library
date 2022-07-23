b6dd07acd6f7239341c77a2ea344f6653faf3165
Steven <stevenxchung@gmail.com>
2022-07-23 07:29:05 -0400

    Create 62-unique-paths.py

---
+++ Blind 75/14 - 2D Dynamic Programming/62-unique-paths.py
@@ -0,0 +1,55 @@
+'''
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+'''
+from time import time
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        cache = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cache[j] = cache[j] + cache[j - 1]
+        return cache[n - 1]
+
+    def reference(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for i in range(m - 1):
+            new_row = [1] * n
+            for j in range(n - 2, -1, -1):
+                new_row[j] = new_row[j + 1] + row[j]
+            row = new_row
+        return row[0]
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.uniquePaths(case[0], case[1]))
+                else:
+                    self.uniquePaths(case[0], case[1])
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case[0], case[1]))
+                else:
+                    self.reference(case[0], case[1])
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        (3, 7),
+        (3, 2)
+    ]
+    test.quantify(test_cases)
