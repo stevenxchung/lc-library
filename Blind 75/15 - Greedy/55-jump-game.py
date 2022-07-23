ea3b55864a82447ddd17db2f2457fd34957ac125
Steven <stevenxchung@gmail.com>
2022-07-23 07:29:16 -0400

    Create 55-jump-game.py

---
+++ Blind 75/15 - Greedy/55-jump-game.py
@@ -0,0 +1,56 @@
+'''
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+'''
+from time import time
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        target = len(nums) - 1
+        # Start at second last element
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= target:
+                target = i
+
+        return target == 0
+
+    def reference(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return goal == 0
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.canJump(case))
+                else:
+                    self.canJump(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        [2, 3, 1, 1, 4],
+        [3, 2, 1, 0, 4],
+        # Additional
+        [0, 2, 3]
+    ]
+    test.quantify(test_cases)
