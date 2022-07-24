48be9dd0eca55847c8babd7135beed340d558531
Steven <stevenxchung@gmail.com>
2022-07-24 10:20:00 -0400

    Create 252-meeting-rooms.py

---
+++ Blind 75/16 - Intervals/252-meeting-rooms.py
@@ -0,0 +1,59 @@
+'''
+*LeetCode premium problem
+'''
+from math import inf
+from time import time
+from typing import List
+
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> int:
+        prev_end = -inf
+        intervals.sort()
+        for start, end in intervals:
+            if prev_end > end:
+                return False
+            else:
+                prev_end = end
+
+        return True
+
+    def reference(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda i: i[0])
+
+        for i in range(1, len(intervals)):
+            i1 = intervals[i - 1]
+            i2 = intervals[i]
+
+            if i1[1] > i2[0]:
+                return False
+        return True
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.canAttendMeetings(case))
+                else:
+                    self.canAttendMeetings(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        # [(0, 30), (5, 10), (15, 20)],
+        # Additional
+        [(5, 10), (15, 20), (0, 30)]
+    ]
+    test.quantify(test_cases)
