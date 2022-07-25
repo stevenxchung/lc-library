385a5fe25741b658f571b46c2421912c31e4a327
Steven <stevenxchung@gmail.com>
2022-07-25 09:12:26 -0400

    Create 56-merge-intervals.py

---
+++ Blind 75/16 - Intervals/56-merge-intervals.py
@@ -0,0 +1,60 @@
+'''
+Given an array of intervals where intervals[i] = [start_i, end_i], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+'''
+from time import time
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        out = []
+        for interval in sorted(intervals, key=lambda i: i[0]):
+            if out and interval[0] <= out[-1][1]:
+                out[-1][1] = max(out[-1][1], interval[1])
+            else:
+                out += interval,
+        return out
+
+    def reference(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda pair: pair[0])
+        output = [intervals[0]]
+
+        for start, end in intervals:
+            lastEnd = output[-1][1]
+
+            if start <= lastEnd:
+                # merge
+                output[-1][1] = max(lastEnd, end)
+            else:
+                output.append([start, end])
+        return output
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.merge(case))
+                else:
+                    self.merge(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        [[1, 3], [2, 6], [8, 10], [15, 18]],
+        [[1, 4], [4, 5]],
+        # Additional
+        [[1, 4], [0, 4]]
+    ]
+    test.quantify(test_cases)
