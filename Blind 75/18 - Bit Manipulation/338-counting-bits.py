97f6b8b6d4f3116205d696de134d6a6673f61b48
Steven <stevenxchung@gmail.com>
2022-07-27 10:02:43 -0400

    Create 338-counting-bits.py

---
+++ Blind 75/18 - Bit Manipulation/338-counting-bits.py
@@ -0,0 +1,51 @@
+'''
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+'''
+from time import time
+from typing import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0]
+        for i in range(1, n + 1):
+            res.append(format(i, 'b').count('1'))
+        return res
+
+    def reference(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.countBits(case))
+                else:
+                    self.countBits(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        2,
+        5,
+    ]
+    test.quantify(test_cases)
