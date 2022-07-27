624affb5a8a26c054cb402c4607f7520f65c9f5f
Steven <stevenxchung@gmail.com>
2022-07-27 10:02:31 -0400

    Create 191-number-of-1-bits.py

---
+++ Blind 75/18 - Bit Manipulation/191-number-of-1-bits.py
@@ -0,0 +1,54 @@
+'''
+Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+Note:
+
+- Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+- In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+'''
+from time import time
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            res += n % 2
+            n = n >> 1
+        return res
+
+    def reference(self, n: int) -> int:
+        res = 0
+        while n:
+            n &= n - 1
+            res += 1
+        return res
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.hammingWeight(case))
+                else:
+                    self.hammingWeight(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        int('00000000000000000000000000001011', 2),
+        int('00000000000000000000000010000000', 2),
+        int('11111111111111111111111111111101', 2)
+    ]
+    test.quantify(test_cases)
