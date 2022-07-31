b413beb7b51d56c382c1530c3c9fafc1ee131454
Steven <stevenxchung@gmail.com>
2022-07-31 07:25:47 -0400

    Create 167-two-sum-II.py

---
+++ NeetCode +75/02 - Two Pointers/167-two-sum-II.py
@@ -0,0 +1,66 @@
+'''
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+'''
+from time import time
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            total = numbers[l] + numbers[r]
+            if total == target:
+                break
+            elif target - numbers[r] < numbers[l]:
+                r -= 1
+            elif target - numbers[l] > numbers[r]:
+                l += 1
+
+        return [l + 1, r + 1]
+
+    def reference(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.twoSum(case[0], case[1]))
+                else:
+                    self.twoSum(case[0], case[1])
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case[0], case[1]))
+                else:
+                    self.reference(case[0], case[1])
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        ([2, 7, 11, 15], 9),
+        ([2, 3, 4], 6),
+        ([-1, 0], -1)
+    ]
+    test.quantify(test_cases)
