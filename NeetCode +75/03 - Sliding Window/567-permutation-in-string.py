6bcd2c231394ab98615d0c637292280156be1d01
Steven <stevenxchung@gmail.com>
2022-08-01 09:50:11 -0400

    Create 567-permutation-in-string.py

---
+++ NeetCode +75/03 - Sliding Window/567-permutation-in-string.py
@@ -0,0 +1,98 @@
+'''
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+'''
+from time import time
+
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        p1, p2 = 0, len(s1) - 1
+        s1_count = {}
+        for c in s1:
+            if c not in s1_count:
+                s1_count[c] = 1
+            else:
+                s1_count[c] += 1
+
+        if len(s2) < len(s1):
+            return False
+
+        while p2 < len(s2):
+            word_count = {}
+            for c in s2[p1:p2 + 1]:
+                if c not in word_count:
+                    word_count[c] = 1
+                else:
+                    word_count[c] += 1
+            if s1_count == word_count:
+                return True
+            p1 += 1
+            p2 += 1
+
+        return False
+
+    def reference(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord("a")] += 1
+            s2Count[ord(s2[i]) - ord("a")] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += 1 if s1Count[i] == s2Count[i] else 0
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+
+            index = ord(s2[r]) - ord("a")
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+
+            index = ord(s2[l]) - ord("a")
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.checkInclusion(case[0], case[1]))
+                else:
+                    self.checkInclusion(case[0], case[1])
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case[0], case[1]))
+                else:
+                    self.reference(case[0], case[1])
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        ('ab', 'eidbaooo'),
+        ('ab', 'eidboaoo'),
+        # Additional
+        ('hello', 'ooolleoooleh')
+    ]
+    test.quantify(test_cases)
