6123952cbcae01a018e443418a63b6a998d3ed48
Steven <stevenxchung@gmail.com>
2022-08-02 08:50:38 -0400

    Create 150-evaluate-reverse-polish-notation.py

---
+++ NeetCode +75/04 - Stack/150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,77 @@
+'''
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+
+Note that division between two integers should truncate toward zero.
+
+It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+'''
+from time import time
+from typing import List
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif c == '-':
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif c == '/':
+                a, b = stack.pop(), stack.pop()
+                stack.append(b // a)
+            else:
+                stack.append(int(c))
+
+        return stack[-1]
+
+    def reference(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.evalRPN(case))
+                else:
+                    self.evalRPN(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        ['2', '1', '+', '3', '*'],
+        ['4', '13', '5', '/', '+'],
+        ['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+']
+    ]
+    test.quantify(test_cases)
