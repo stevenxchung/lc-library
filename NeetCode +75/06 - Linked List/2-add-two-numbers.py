33faa158855684bbb60c00de6629439bb668fa1e
Steven <stevenxchung@gmail.com>
2022-08-16 08:59:25 -0400

    Create 2-add-two-numbers.py

---
+++ NeetCode +75/06 - Linked List/2-add-two-numbers.py
@@ -0,0 +1,95 @@
+'''
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+'''
+from time import time
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        node = ListNode()
+        res = node
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+
+            node.next = ListNode(val)
+            node = node.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return res.next
+
+    def reference(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            # New digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            # Update pointers
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.addTwoNumbers(case[0], case[1]))
+                else:
+                    self.addTwoNumbers(case[0], case[1])
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case[0], case[1]))
+                else:
+                    self.reference(case[0], case[1])
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        (
+            ListNode(2, ListNode(4, ListNode(3))),
+            ListNode(5, ListNode(6, ListNode(4)))
+        ),
+        (
+            ListNode(0),
+            ListNode(0)
+        ),
+        (
+            ListNode(9, ListNode(9, ListNode(9, ListNode(
+                9, ListNode(9, ListNode(9, ListNode(9))))))),
+            ListNode(9, ListNode(9, ListNode(9, ListNode(9))))
+        )
+    ]
+    test.quantify(test_cases)
