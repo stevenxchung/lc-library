d37f3d1f3135b7a934b10fb1966001aecd5cde7d
Steven <stevenxchung@gmail.com>
2022-08-22 08:32:09 -0400

    Create 199-binary-tree-right-side-view.py

---
+++ NeetCode +75/07 - Trees/199-binary-tree-right-side-view.py
@@ -0,0 +1,84 @@
+'''
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+'''
+import collections
+from time import time
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def dfs(node: Optional[TreeNode], level: int):
+            if not node:
+                return
+            if len(res) == level:
+                res.append(node.val)
+
+            dfs(node.right, level + 1)
+            dfs(node.left, level + 1)
+
+        dfs(root, 0)
+        return res
+
+    def reference(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = collections.deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        return res
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.rightSideView(case))
+                else:
+                    self.rightSideView(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        TreeNode(1,
+                 TreeNode(2, None, TreeNode(5)),
+                 TreeNode(3, None, TreeNode(4))
+                 ),
+        TreeNode(1, None, TreeNode(3)),
+        None
+    ]
+    test.quantify(test_cases)
