5e143c10f73a2354b7d7227458b75c6794f9a295
Steven <stevenxchung@gmail.com>
2022-08-24 10:22:41 -0400

    Create 1046-last-stone-weight.py

---
+++ NeetCode +75/09 - Heap, Priority Queue/1046-last-stone-weight.py
@@ -0,0 +1,71 @@
+'''
+You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+- If x == y, both stones are destroyed, and
+- If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+
+At the end of the game, there is at most one stone left.
+
+Return the weight of the last remaining stone. If there are no stones left, return 0.
+'''
+import heapq
+from time import time
+from typing import List
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        max_heap = [-1 * stone for stone in stones]
+        heapq.heapify(max_heap)
+        while len(max_heap) > 1:
+            stone1 = heapq.heappop(max_heap)
+            stone2 = heapq.heappop(max_heap)
+            new_stone = abs(stone1 - stone2)
+            heapq.heappush(max_heap, -new_stone)
+
+        return abs(max_heap[0])
+
+    def reference(self, stones: List[int]) -> int:
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+            if second > first:
+                heapq.heappush(stones, first - second)
+
+        stones.append(0)
+        return abs(stones[0])
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.lastStoneWeight(case))
+                else:
+                    self.lastStoneWeight(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        [2, 7, 4, 1, 8, 1],
+        [1],
+        # Additional
+        [3, 7, 2]
+    ]
+    test.quantify(test_cases)
