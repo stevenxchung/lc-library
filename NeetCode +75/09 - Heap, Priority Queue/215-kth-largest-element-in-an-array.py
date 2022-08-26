2c7c2f020a99c691eef1ae1e44a37e5467bc323d
Steven <stevenxchung@gmail.com>
2022-08-26 07:50:47 -0400

    Create 215-kth-largest-element-in-an-array.py

---
+++ NeetCode +75/09 - Heap, Priority Queue/215-kth-largest-element-in-an-array.py
@@ -0,0 +1,77 @@
+'''
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+You must solve it in O(n) time complexity.
+'''
+import heapq
+from time import time
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        new_nums = [-n for n in nums]
+        heapq.heapify(nums)
+        res = heapq.nsmallest(k, new_nums)
+
+        return -res[-1]
+
+    def partition(self, nums: List[int], left: int, right: int) -> int:
+        pivot, fill = nums[right], left
+
+        for i in range(left, right):
+            if nums[i] <= pivot:
+                nums[fill], nums[i] = nums[i], nums[fill]
+                fill += 1
+
+        nums[fill], nums[right] = nums[right], nums[fill]
+
+        return fill
+
+    def reference(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            pivot = self.partition(nums, left, right)
+
+            if pivot < k:
+                left = pivot + 1
+            elif pivot > k:
+                right = pivot - 1
+            else:
+                break
+
+        return nums[k]
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.findKthLargest(case[0], case[1]))
+                else:
+                    self.findKthLargest(case[0], case[1])
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case[0], case[1]))
+                else:
+                    self.reference(case[0], case[1])
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        ([3, 2, 1, 5, 6, 4], 2),
+        ([3, 2, 3, 1, 2, 4, 5, 5, 6], 4),
+        # Additional
+        ([-1, -1], 2)
+    ]
+    test.quantify(test_cases)
