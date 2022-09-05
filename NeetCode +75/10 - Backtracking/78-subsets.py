36378d4e7845ecb723e7ac8d7159a89ac74fab9d
Steven <stevenxchung@gmail.com>
2022-09-05 09:10:12 -0400

    Create 78-subsets.py

---
+++ NeetCode +75/10 - Backtracking/78-subsets.py
@@ -0,0 +1,70 @@
+'''
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+'''
+from time import time
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(i: int, subset: List[int]):
+            if i == len(nums):
+                res.append(subset.copy())
+                return
+
+            dfs(i + 1, subset + [nums[i]])
+            dfs(i + 1, subset)
+
+        dfs(0, [])
+        return res
+
+    def reference(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+            # decision NOT to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.subsets(case))
+                else:
+                    self.subsets(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        [1, 2, 3],
+        [0]
+    ]
+    test.quantify(test_cases)
