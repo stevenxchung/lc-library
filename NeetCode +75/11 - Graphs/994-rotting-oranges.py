c82dae53832a07dd9a1243f7f07652832f1c5277
Steven <stevenxchung@gmail.com>
2022-09-13 08:47:27 -0400

    Create 994-rotting-oranges.py

---
+++ NeetCode +75/11 - Graphs/994-rotting-oranges.py
@@ -0,0 +1,117 @@
+'''
+You are given an m x n grid where each cell can have one of three values:
+
+- 0 representing an empty cell,
+- 1 representing a fresh orange, or
+- 2 representing a rotten orange.
+
+Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+'''
+import collections
+from time import time
+from typing import List
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        queue = []
+        fresh = 0
+        time = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+
+        dir = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        while queue and fresh > 0:
+            # Start with rotten oranges
+            for i in range(len(queue)):
+                r, c = queue.pop(0)
+
+                # Check adjacent oranges
+                for dr, dc in dir:
+                    row = r + dr
+                    col = c + dc
+                    if row in range(ROWS) and col in range(COLS) and grid[row][col] == 1:
+                        grid[row][col] = 2
+                        queue.append((row, col))
+                        fresh -= 1
+            time += 1
+        return time if fresh == 0 else -1
+
+    def reference(self, grid: List[List[int]]) -> int:
+        q = collections.deque()
+        fresh = 0
+        time = 0
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2:
+                    q.append((r, c))
+
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        while fresh > 0 and q:
+            length = len(q)
+            for i in range(length):
+                r, c = q.popleft()
+
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+                    # if in bounds and nonrotten, make rotten
+                    # and add to q
+                    if (
+                        row in range(len(grid))
+                        and col in range(len(grid[0]))
+                        and grid[row][col] == 1
+                    ):
+                        grid[row][col] = 2
+                        q.append((row, col))
+                        fresh -= 1
+            time += 1
+        return time if fresh == 0 else -1
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.orangesRotting(case))
+                else:
+                    self.orangesRotting(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        [
+            [2, 1, 1],
+            [1, 1, 0],
+            [0, 1, 1]
+        ],
+        [
+            [2, 1, 1],
+            [0, 1, 1],
+            [1, 0, 1]
+        ],
+        [
+            [0, 2]
+        ]
+    ]
+    test.quantify(test_cases)
