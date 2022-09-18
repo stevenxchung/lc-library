cf402ae64ebc05a3c932af33dc87cefa94d048b3
Steven <stevenxchung@gmail.com>
2022-09-18 07:25:10 -0400

    Create 787-cheapest-flights-within-k-stops.py

---
+++ NeetCode +75/12 - Advanced Graphs/787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,90 @@
+'''
+There are n cities connected by some number of flights. You are given an array flights where flights[i] = [from_i, to_i, price_i] indicates that there is a flight from city from_i to city to_i with cost price_i.
+
+You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
+'''
+from math import inf
+from time import time
+from typing import List
+
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [inf] * n
+        prices[src] = 0
+
+        for i in range(k + 1):
+            temp_prices = prices[:]
+            for s, d, p in flights:
+                if prices[s] == inf:
+                    # Not possible to reach
+                    continue
+                if prices[s] + p < temp_prices[d]:
+                    # Found new shortest path
+                    temp_prices[d] = prices[s] + p
+            prices = temp_prices
+
+        return -1 if prices[dst] == inf else prices[dst]
+
+    def reference(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float("inf")] * n
+        prices[src] = 0
+
+        for i in range(k + 1):
+            tmpPrices = prices.copy()
+
+            for s, d, p in flights:  # s=source, d=dest, p=price
+                if prices[s] == float("inf"):
+                    continue
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
+            prices = tmpPrices
+
+        return -1 if prices[dst] == float("inf") else prices[dst]
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.findCheapestPrice(*case))
+                else:
+                    self.findCheapestPrice(*case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(*case))
+                else:
+                    self.reference(*case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        (
+            4,
+            [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]],
+            0,
+            3,
+            1
+        ),
+        (
+            3,
+            [[0, 1, 100], [1, 2, 100], [0, 2, 500]],
+            0,
+            2,
+            1
+        ),
+        (
+            3,
+            [[0, 1, 100], [1, 2, 100], [0, 2, 500]],
+            0,
+            2,
+            0
+        )
+    ]
+    test.quantify(test_cases)
