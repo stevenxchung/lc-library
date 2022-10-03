1859ecaf3edad44837db6b91a1de3580763eeba4
Steven <stevenxchung@gmail.com>
2022-10-02 20:39:12 -0400

    Update 295-find-median-from-data-stream.py

--- Blind 75/09 - Heap, Priority Queue/295-find-median-from-data-stream.py
+++ Blind 75/09 - Heap, Priority Queue/295-find-median-from-data-stream.py
@@ -15,10 +15,10 @@ from time import time
 
 
 class MedianFinder:
-
-    def __init__(self):
+    def __init__(self, debug=False):
         # Max heap and min heap respectively
         self.small, self.large = [], []
+        self.debug = debug
 
     def addNum(self, num: int) -> None:
         # Add to max heap first
@@ -37,20 +37,23 @@ class MedianFinder:
     def findMedian(self) -> float:
         if len(self.small) > len(self.large):
             res = -self.small[0]
-            print(res)
+            if self.debug:
+                print(res)
             return res
         elif len(self.small) < len(self.large):
             res = self.large[0]
-            print(res)
+            if self.debug:
+                print(res)
             return res
         # Otherwise take average
         res = float(self.large[0] - self.small[0]) / 2
-        print(res)
+        if self.debug:
+            print(res)
         return res
 
 
 if __name__ == '__main__':
-    test = MedianFinder()
+    test = MedianFinder(debug=True)
     sol_start = time()
     test.addNum(1)    # arr = [1]
     test.addNum(2)    # arr = [1, 2]
