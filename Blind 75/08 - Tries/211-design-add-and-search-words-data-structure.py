1604ae33a502ed4a647a812a56aa922346dbbe37
Steven <stevenxchung@gmail.com>
2022-10-08 07:55:19 -0400

    Update 211-design-add-and-search-words-data-structure.py

--- Blind 75/08 - Tries/211-design-add-and-search-words-data-structure.py
+++ Blind 75/08 - Tries/211-design-add-and-search-words-data-structure.py
@@ -3,9 +3,9 @@ Design a data structure that supports adding new words and finding if a string m
 
 Implement the test class:
 
-WordDictionary() Initializes the object.
-void addWord(word) Adds word to the data structure, it can be matched later.
-bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+- WordDictionary() Initializes the object.
+- void addWord(word) Adds word to the data structure, it can be matched later.
+- bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
 '''
 from time import time
 
@@ -30,25 +30,25 @@ class WordDictionary:
             node = node.children[c]
         node.is_end = True
 
-    def dfs_helper(self, word: str, position: int, root: TrieNode):
-        node = root
-        for i in range(position, len(word)):
-            c = word[i]
-            if c == '.':
-                for child in node.children.values():
-                    if self.dfs_helper(word, i + 1, child):
-                        # Found word
-                        return True
-                    return False
-            else:
-                if c not in node.children:
-                    return False
+    def search(self, word: str) -> bool:
+
+        def dfs(word, start, curr):
+            node = curr
+            for i in range(start, len(word)):
+                c = word[i]
+                if c == '.':
+                    for child in node.children.values():
+                        if dfs(word, i + 1, child):
+                            return True
+                        return False
+                else:
+                    if c not in node.children:
+                        return False
                 node = node.children[c]
 
-        return node.is_end
+            return node.is_end
 
-    def search(self, word: str) -> bool:
-        res = self.dfs_helper(word, 0, self.root)
+        res = dfs(word, 0, self.root)
         print(res)
         return res
 
@@ -56,11 +56,11 @@ class WordDictionary:
 if __name__ == '__main__':
     test = WordDictionary()
     sol_start = time()
-    test.addWord("bad")
-    test.addWord("dad")
-    test.addWord("mad")
-    test.search("pad")  # return False
-    test.search("bad")  # return True
-    test.search(".ad")  # return True
-    test.search("b..")  # return True
+    test.addWord('bad')
+    test.addWord('dad')
+    test.addWord('mad')
+    test.search('pad')  # return False
+    test.search('bad')  # return True
+    test.search('.ad')  # return True
+    test.search('b..')  # return True
     print(f'Runtime for our solution: {time() - sol_start}')
