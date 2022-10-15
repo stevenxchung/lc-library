0ce61b207ca379f5af1c5c8e56c5018f5664ad28
Steven <stevenxchung@gmail.com>
2022-10-15 08:16:02 -0400

    Create 300-longest-increasing-subsequence.py

---
+++ Blind 75/13 - 1D Dynamic Programming/300-longest-increasing-subsequence.py
@@ -0,0 +1,62 @@
+'''
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+'''
+from time import time
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] < x:
+                sub.append(x)
+            else:
+                # Find the index of the smallest number >= x
+                idx = 0
+                for i, n in enumerate(sub):
+                    if sub[i] >= x:
+                        idx = i
+                        break
+                sub[idx] = x  # Replace that number with x
+        return len(sub)
+
+    def reference(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+        return max(LIS)
+
+    def quantify(self, test_cases, runs=100000):
+        sol_start = time()
+        for i in range(runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.lengthOfLIS(case))
+                else:
+                    self.lengthOfLIS(case)
+        print(f'Runtime for our solution: {time() - sol_start}')
+
+        ref_start = time()
+        for i in range(0, runs):
+            for case in test_cases:
+                if i == 0:
+                    print(self.reference(case))
+                else:
+                    self.reference(case)
+        print(f'Runtime for reference: {time() - ref_start}')
+
+
+if __name__ == '__main__':
+    test = Solution()
+    test_cases = [
+        [10, 9, 2, 5, 3, 7, 101, 18],
+        [0, 1, 0, 3, 2, 3],
+        [7, 7, 7, 7, 7, 7, 7]
+    ]
+    test.quantify(test_cases)
