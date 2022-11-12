629b2f8f4f08a643f698e41b051678e62d3e0e07
Steven <stevenxchung@gmail.com>
2022-11-12 08:00:42 -0500

    Update 128-longest-consecutive-sequence.py

--- Blind 75/01 - Arrays and Hashing/128-longest-consecutive-sequence.py
+++ Blind 75/01 - Arrays and Hashing/128-longest-consecutive-sequence.py
@@ -38,7 +38,7 @@ class Solution:
                 longest = max(length, longest)
         return longest
 
-    def quantify(self, test_cases, runs=100000):
+    def quantify(self, test_cases, runs=50000):
         sol_start = time()
         for i in range(runs):
             for case in test_cases:
