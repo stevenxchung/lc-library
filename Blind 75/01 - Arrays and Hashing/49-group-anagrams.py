b9fd8a7d897f2c49c85fba3eeb4402bba189cf1a
Steven <stevenxchung@gmail.com>
2022-11-17 09:26:44 -0500

    Update 49-group-anagrams.py

--- Blind 75/01 - Arrays and Hashing/49-group-anagrams.py
+++ Blind 75/01 - Arrays and Hashing/49-group-anagrams.py
@@ -35,7 +35,7 @@ class Solution:
             ans[tuple(count)].append(s)
         return ans.values()
 
-    def quantify(self, test_cases, runs=100000):
+    def quantify(self, test_cases, runs=50000):
         sol_start = time()
         for i in range(runs):
             for case in test_cases:
