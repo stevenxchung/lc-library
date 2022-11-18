23cbe75f677525d79d9ba0da196843b9728e14d9
Steven <stevenxchung@gmail.com>
2022-11-18 08:36:45 -0500

    Update 659-encode-and-decode-strings.py

--- Blind 75/01 - Arrays and Hashing/659-encode-and-decode-strings.py
+++ Blind 75/01 - Arrays and Hashing/659-encode-and-decode-strings.py
@@ -53,7 +53,7 @@ class Solution:
             i = j + 1 + length
         return res
 
-    def quantify(self, test_cases, runs=100000):
+    def quantify(self, test_cases, runs=50000):
         sol_start = time()
         for i in range(runs):
             for case in test_cases:
