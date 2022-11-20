d79625f1ada4501d29b0c6e98f3daca142bc0ca2
Steven <stevenxchung@gmail.com>
2022-11-20 08:00:52 -0500

    Update 125-valid-palindrome.py

--- Blind 75/02 - Two Pointers/125-valid-palindrome.py
+++ Blind 75/02 - Two Pointers/125-valid-palindrome.py
@@ -39,7 +39,7 @@ class Solution:
                 ord('a') <= ord(c) <= ord('z') or
                 ord('0') <= ord(c) <= ord('9'))
 
-    def quantify(self, test_cases, runs=100000):
+    def quantify(self, test_cases, runs=50000):
         sol_start = time()
         for i in range(runs):
             for case in test_cases:
