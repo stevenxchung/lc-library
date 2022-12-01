bdf3a4ce13c6d36c024bcf18013c12ef2dbd4643
Steven <stevenxchung@gmail.com>
2022-12-01 09:32:54 -0500

    Update 206-reverse-linked-list.py

--- Blind 75/06 - Linked List/206-reverse-linked-list.py
+++ Blind 75/06 - Linked List/206-reverse-linked-list.py
@@ -36,7 +36,7 @@ class Solution:
 
         return prev
 
-    def quantify(self, test_cases, runs=10000):
+    def quantify(self, test_cases, runs=50000):
         sol_start = time()
         for i in range(runs):
             for case in test_cases:
