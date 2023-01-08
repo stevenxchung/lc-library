b914f2af62199d96d94237756d9e35851772fd05
Steven <stevenxchung@gmail.com>
2023-01-08 09:43:59 -0500

    Update 347-top-k-frequent-elements.py

--- Blind 75/01 - Arrays and Hashing/347-top-k-frequent-elements.py
+++ Blind 75/01 - Arrays and Hashing/347-top-k-frequent-elements.py
@@ -1,15 +1,13 @@
 '''
 Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 '''
-from collections import defaultdict
 from time import time
 from typing import List
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        res = []
-        freq = defaultdict()
+        res, freq = [], {}
         for n in nums:
             freq[n] = 1 + freq.get(n, 0)
 
@@ -42,25 +40,22 @@ class Solution:
         for i in range(runs):
             for case in test_cases:
                 if i == 0:
-                    print(self.topKFrequent(case[0], case[1]))
+                    print(self.topKFrequent(*case))
                 else:
-                    self.topKFrequent(case[0], case[1])
+                    self.topKFrequent(*case)
         print(f'Runtime for our solution: {time() - sol_start}')
 
         ref_start = time()
         for i in range(0, runs):
             for case in test_cases:
                 if i == 0:
-                    print(self.reference(case[0], case[1]))
+                    print(self.reference(*case))
                 else:
-                    self.reference(case[0], case[1])
+                    self.reference(*case)
         print(f'Runtime for reference: {time() - ref_start}')
 
 
 if __name__ == '__main__':
     test = Solution()
-    test_cases = [
-        ([1, 1, 1, 2, 2, 3], 2),
-        ([1], 1)
-    ]
+    test_cases = [([1, 1, 1, 2, 2, 3], 2), ([1], 1)]
     test.quantify(test_cases)
