3dd306455aa23e386d22852c5c1229ec145a7519
Steven <stevenxchung@gmail.com>
2023-02-07 19:24:52 -0500

    Update 152-maximum-product-subarray.py

--- Blind 75/13 - 1D Dynamic Programming/152-maximum-product-subarray.py
+++ Blind 75/13 - 1D Dynamic Programming/152-maximum-product-subarray.py
@@ -11,26 +11,22 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        '''
-        :type nums: List[int]
-        :rtype: int
-        '''
         res = nums[0]
-        current = 1
+        curr = 1
         # Left to right pass. Captures all potential subarrays containing first odd n negative numbers
-        for i in nums:
-            current *= i
-            res = max(res, current)
-            # Zero is a delimiter, restart at 1. This is optimal since zero multiplied on is still zero.
-            if current == 0:
-                current = 1
-        current = 1
+        for n in nums:
+            curr *= n
+            res = max(res, curr)
+            # 0 is a delimiter, restart at 1. This is optimal since 0 * n is still 0.
+            if curr == 0:
+                curr = 1
+        curr = 1
         # Right to left pass capturing all potential subarrays containing last odd n negative numbers
-        for i in reversed(nums):
-            current *= i
-            res = max(res, current)
-            if current == 0:
-                current = 1
+        for n in reversed(nums):
+            curr *= n
+            res = max(res, curr)
+            if curr == 0:
+                curr = 1
         return res
 
     def reference(self, nums: List[int]) -> int:
@@ -39,9 +35,9 @@ class Solution:
         curMin, curMax = 1, 1
 
         for n in nums:
-            tmp = curMax * n
-            curMax = max(n * curMax, n * curMin, n)
-            curMin = min(tmp, n * curMin, n)
+            curMax, curMin = max(n * curMax, n * curMin, n), min(
+                n * curMax, n * curMin, n
+            )
             res = max(res, curMax)
 
         return res
