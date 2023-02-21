20d5b6a43cddb35053da28b1c73b87ee8a7cf889
Steven <stevenxchung@gmail.com>
2023-02-21 08:04:37 -0500

    Update 2-add-two-numbers.py

--- NeetCode +75/06 - Linked List/2-add-two-numbers.py
+++ NeetCode +75/06 - Linked List/2-add-two-numbers.py
@@ -39,7 +39,7 @@ class Solution:
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
         dummy = ListNode()
-        cur = dummy
+        curr = dummy
 
         carry = 0
         while l1 or l2 or carry:
@@ -50,10 +50,10 @@ class Solution:
             val = v1 + v2 + carry
             carry = val // 10
             val = val % 10
-            cur.next = ListNode(val)
+            curr.next = ListNode(val)
 
             # Update pointers
-            cur = cur.next
+            curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
 
