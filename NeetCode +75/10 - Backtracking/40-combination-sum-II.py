152bf854270353bdf552d6833cfafe7269700d8b
Steven <stevenxchung@gmail.com>
2023-02-23 08:54:25 -0500

    Update 40-combination-sum-II.py

--- NeetCode +75/10 - Backtracking/40-combination-sum-II.py
+++ NeetCode +75/10 - Backtracking/40-combination-sum-II.py
@@ -39,9 +39,9 @@ class Solution:
 
         res = []
 
-        def backtrack(cur, pos, target):
+        def backtrack(curr, pos, target):
             if target == 0:
-                res.append(cur.copy())
+                res.append(curr.copy())
             if target <= 0:
                 return
 
@@ -49,9 +49,9 @@ class Solution:
             for i in range(pos, len(candidates)):
                 if candidates[i] == prev:
                     continue
-                cur.append(candidates[i])
-                backtrack(cur, i + 1, target - candidates[i])
-                cur.pop()
+                curr.append(candidates[i])
+                backtrack(curr, i + 1, target - candidates[i])
+                curr.pop()
                 prev = candidates[i]
 
         backtrack([], 0, target)
