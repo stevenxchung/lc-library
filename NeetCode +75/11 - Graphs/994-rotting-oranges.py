895a1b62905b865651fcaeb4d8e2a0b2bdebdc44
Steven <stevenxchung@gmail.com>
2023-02-26 14:37:54 -0500

    Update 994-rotting-oranges.py

--- NeetCode +75/11 - Graphs/994-rotting-oranges.py
+++ NeetCode +75/11 - Graphs/994-rotting-oranges.py
@@ -18,36 +18,33 @@ from typing import List
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
+        fresh, time = 0, 0
         queue = []
-        fresh = 0
-        time = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
                     fresh += 1
-                if grid[r][c] == 2:
+                elif grid[r][c] == 2:
                     queue.append((r, c))
 
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         while queue and fresh > 0:
-            # Start with rotten oranges
-            for i in range(len(queue)):
-                r, c = queue.pop(0)
-
-                # Check adjacent oranges
+            for _ in queue:
+                r1, c1 = queue.pop(0)
                 for dr, dc in directions:
-                    row = r + dr
-                    col = c + dc
+                    r2, c2 = r1 + dr, c1 + dc
                     if (
-                        row in range(ROWS)
-                        and col in range(COLS)
-                        and grid[row][col] == 1
+                        r2 not in range(ROWS)
+                        or c2 not in range(COLS)
+                        or grid[r2][c2] != 1
                     ):
-                        grid[row][col] = 2
-                        queue.append((row, col))
-                        fresh -= 1
+                        continue
+                    queue.append((r2, c2))
+                    grid[r2][c2] = 2
+                    fresh -= 1
             time += 1
-        return time if fresh == 0 else -1
+
+        return -1 if fresh > 0 else time
 
     def reference(self, grid: List[List[int]]) -> int:
         q = collections.deque()
@@ -80,6 +77,7 @@ class Solution:
                         q.append((row, col))
                         fresh -= 1
             time += 1
+
         return time if fresh == 0 else -1
 
     def quantify(self, test_cases, runs=50000):
