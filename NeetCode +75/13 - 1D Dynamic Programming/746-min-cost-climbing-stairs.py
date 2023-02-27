3567fcb8d9aee4054764ecdb64fcab0563ae3132
Steven <stevenxchung@gmail.com>
2023-02-27 08:32:25 -0500

    Update 746-min-cost-climbing-stairs.py

--- NeetCode +75/13 - 1D Dynamic Programming/746-min-cost-climbing-stairs.py
+++ NeetCode +75/13 - 1D Dynamic Programming/746-min-cost-climbing-stairs.py
@@ -5,38 +5,43 @@ You can either start from the step with index 0, or the step with index 1.
 
 Return the minimum cost to reach the top of the floor.
 '''
+from copy import deepcopy
 from time import time
 from typing import List
 
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost.append(0)
         for i in range(len(cost) - 3, -1, -1):
             cost[i] += min(cost[i + 1], cost[i + 2])
 
         return min(cost[0], cost[1])
 
     def reference(self, cost: List[int]) -> int:
-        return
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+
+        return min(cost[0], cost[1])
 
     def quantify(self, test_cases, runs=50000):
         sol_start = time()
         for i in range(runs):
             for case in test_cases:
+                copy = deepcopy(case)
                 if i == 0:
-                    print(self.minCostClimbingStairs(case))
+                    print(self.minCostClimbingStairs(copy))
                 else:
-                    self.minCostClimbingStairs(case)
+                    self.minCostClimbingStairs(copy)
         print(f'Runtime for our solution: {time() - sol_start}\n')
 
         ref_start = time()
         for i in range(0, runs):
             for case in test_cases:
+                copy = deepcopy(case)
                 if i == 0:
-                    print(self.reference(case))
+                    print(self.reference(copy))
                 else:
-                    self.reference(case)
+                    self.reference(copy)
         print(f'Runtime for reference: {time() - ref_start}')
 
 
