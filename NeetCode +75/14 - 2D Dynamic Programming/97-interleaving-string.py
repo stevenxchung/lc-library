32a2f552e923cbb6384312e929e6bab5d8a7e4e1
Steven <stevenxchung@gmail.com>
2023-03-05 08:51:02 -0500

    Update 97-interleaving-string.py

--- NeetCode +75/14 - 2D Dynamic Programming/97-interleaving-string.py
+++ NeetCode +75/14 - 2D Dynamic Programming/97-interleaving-string.py
@@ -18,17 +18,23 @@ class Solution:
         if len(s1) + len(s2) != len(s3):
             return False
 
-        cache = [[False] * (len(s2) + 1) for i in range(len(s1) + 1)]
-        cache[len(s1)][len(s2)] = True
+        cache = {}  # {(i, j) : T/F}
 
-        for i in range(len(s1), -1, -1):
-            for j in range(len(s2), -1, -1):
-                if i < len(s2) and s1[i] == s3[i + j] and cache[i + 1][j]:
-                    cache[i][j] = True
-                if j < len(s2) and s2[j] == s3[i + j] and cache[i][j + 1]:
-                    cache[i][j] = True
+        def dfs(i, j):
+            if i == len(s1) and j == len(s2):
+                return True
+            if (i, j) in cache:
+                return cache[(i, j)]
 
-        return cache[0][0]
+            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+                return True
+            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+                return True
+
+            cache[(i, j)] = False
+            return cache[(i, j)]
+
+        return dfs(0, 0)
 
     def reference(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3):
