45c52122bea3ae01195b150b4ff928f1f0c2a63c
Steven <stevenxchung@gmail.com>
2023-04-06 09:27:29 -0400

    Update 17-letter-combinations-of-a-phone-number.py

--- NeetCode +75/10 - Backtracking/17-letter-combinations-of-a-phone-number.py
+++ NeetCode +75/10 - Backtracking/17-letter-combinations-of-a-phone-number.py
@@ -10,29 +10,29 @@ from typing import List
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         phone_map = {
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z'],
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
         }
+
         res = []
-        if len(digits) == 0:
-            return res
 
-        def dfs(i: int, curr: List[str]):
-            if len(digits) == len(curr):
-                res.append(''.join(curr))
+        def dfs(i, substr):
+            if i >= len(digits):
+                res.append(substr)
                 return
-            for c in phone_map[digits[i]]:
-                curr.append(c)
-                dfs(i + 1, curr)
-                curr.pop()
 
-        dfs(0, [])
+            for c in phone_map[digits[i]]:
+                dfs(i + 1, substr + c)
+
+        if len(digits) > 0:
+            dfs(0, '')
+
         return res
 
     def reference(self, digits: str) -> List[str]:
