0919e16157f622c68100aec7c2bed73e3841d15f
Steven <stevenxchung@gmail.com>
2023-12-18 09:40:13 -0500

    Update 206-reverse-linked-list.py

--- 01 - Easy/206-reverse-linked-list.py
+++ 01 - Easy/206-reverse-linked-list.py
@@ -4,9 +4,9 @@ Reverse a singly linked list.
 
 
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 # Iterative
