82a0bafaa8997e26a8abf543528a4987b30a199d
Steven <stevenxchung@gmail.com>
2024-05-19 09:05:45 -0400

    Update 239-sliding-window-maximum.py

--- NeetCode +75/03 - Sliding Window/239-sliding-window-maximum.py
+++ NeetCode +75/03 - Sliding Window/239-sliding-window-maximum.py
@@ -13,21 +13,21 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         res = []
         # Monotonically decreasing queue
-        mono_i_q = collections.deque([])
+        mono_q = collections.deque([])
         l = 0
         for r in range(len(nums)):
             # Remove smaller values from queue
-            while mono_i_q and nums[r] > nums[mono_i_q[0]]:
-                mono_i_q.popleft()
-            mono_i_q.appendleft(r)
+            while mono_q and nums[r] > nums[mono_q[0]]:
+                mono_q.popleft()
+            mono_q.appendleft(r)
 
             # Remove element index if outside window
-            if l > mono_i_q[-1]:
-                mono_i_q.pop()
+            if l > mono_q[-1]:
+                mono_q.pop()
 
             # Add if window has k elements
             if (r - l + 1) == k:
-                res.append(nums[mono_i_q[-1]])
+                res.append(nums[mono_q[-1]])
                 l += 1
 
         return res
