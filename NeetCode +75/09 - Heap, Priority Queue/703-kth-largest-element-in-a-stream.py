636462f93069e65348205b1b59b136f44f1d7b2a
Steven <stevenxchung@gmail.com>
2024-06-23 15:56:14 -0400

    Update 703-kth-largest-element-in-a-stream.py

--- NeetCode +75/09 - Heap, Priority Queue/703-kth-largest-element-in-a-stream.py
+++ NeetCode +75/09 - Heap, Priority Queue/703-kth-largest-element-in-a-stream.py
@@ -6,18 +6,20 @@ Implement KthLargest class:
 - KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
 - int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
 '''
+
 import heapq
 from time import time
 from typing import List
 
 
 class KthLargest:
-    def __init__(self, k: int, nums: List[int]):
+    def __init__(self, k: int, nums: List[int], debug=False):
+        self.debug = debug
         self.k = k
         self.min_heap = nums
         heapq.heapify(self.min_heap)
+        # Limit heap size to k to keep kth largest at top
         while len(self.min_heap) > k:
-            # Remove minium value
             heapq.heappop(self.min_heap)
 
     def add(self, val: int) -> int:
@@ -25,16 +27,17 @@ class KthLargest:
         heapq.heappush(self.min_heap, val)
         if len(self.min_heap) > self.k:
             heapq.heappop(self.min_heap)
-        # Kth largest is always on the top
+        if self.debug:
+            print(f'add({val}): {self.min_heap[0]}')
         return self.min_heap[0]
 
 
 if __name__ == '__main__':
-    test = KthLargest(3, [4, 5, 8, 2])
+    test = KthLargest(3, [4, 5, 8, 2], debug=True)
     sol_start = time()
-    test.add(3)  # return 4
-    test.add(5)  # return 5
-    test.add(10)  # return 5
-    test.add(9)  # return 8
-    test.add(4)  # return 8
+    test.add(3)  # Return 4
+    test.add(5)  # Return 5
+    test.add(10)  # Return 5
+    test.add(9)  # Return 8
+    test.add(4)  # Return 8
     print(f'Runtime for our solution: {time() - sol_start}\n')
